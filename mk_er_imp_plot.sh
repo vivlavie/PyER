8dc511f4d2fd2a25bf94404241c1bbe0bc250847
Sung-Hoon Kim <dr.sunghoon.kim@gmail.com>
2020-05-13 16:45:54 +0900

    ER imp routes with FWD right-hand side

--- mk_er_imp_plot.sh
+++ mk_er_imp_plot.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-for f in `ls J12*er_imp.kfx`;do
+for f in `ls J*er_imp.kfx`;do
 #for f in J12;do
 	JXX=J${f:1:2}
 	echo ${JXX}
-	cp temp_er_imp.jpg.s0 ${JXX}_er_imp.jpg.s0
-	sed -e "s/%JXX%/${JXX}/" temp_er_imp.jpg.s0 > ${JXX}_er_imp.jpg.s0
+	#cp temp_er_imp2.jpg.s0 ${JXX}_er_imp.jpg.s0
+	sed -e "s/%JXX%/${JXX}/" temp_er_imp2.jpg.s0 > ${JXX}_er_imp.jpg.s0
 	kfxview ${JXX}_er_imp.jpg.s0
 done
 
